233c6b3bb7c7c3510248e42ea73df5e38c1b2f5c
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-12-12 16:13:28 -0600

    init server and knex

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const recipesRouter = require("../recipes/recipes-router.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.use("/api/recipes", recipesRouter);
+
+module.exports = server;
